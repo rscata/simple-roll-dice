397fc7c9cd39d70660521aaa43b3e27153b15c09
cata <catalin.rscata@gmail.com>
2020-03-24 17:58:29 +0200

    1.0.4

--- src/__tests__/DiceMod.test.ts
+++ src/__tests__/DiceMod.test.ts
@@ -1,6 +1,6 @@
 import { DiceMod } from '../DiceMod';
 
 test('DiceMod', () => {
-    const inst = new DiceMod();
-    expect(inst.roll('2d6')).toHaveProperty('sides');
-  });
+  const inst = new DiceMod();
+  expect(inst.roll('2d6')).toHaveProperty('sides');
+});
